0455b2581d8a5e81eb92405a7feab899cdd82192
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2014-01-22 09:27:12 +0100

    Add missing class ResultValidator

---
+++ src/main/java/bpp/executables/ResultValidator.java
@@ -0,0 +1,33 @@
+package bpp.executables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bpp.domain.AnalysisResult;
+
+public class ResultValidator {
+
+	public boolean hasMissingValues(AnalysisResult analysisResult) {
+		List<Double> valuesToCheck = new ArrayList<>(5);
+
+		valuesToCheck.add(new Double(analysisResult.getNumberOfElements()));
+		valuesToCheck.add(analysisResult.getActivityPortabilityMetric());
+		valuesToCheck.add(analysisResult.getBasicPortabilityMetric());
+		valuesToCheck.add(analysisResult
+				.getServiceCommunicationPortabilityMetric());
+		valuesToCheck.add(analysisResult.getWeightedElementsMetric());
+
+		for (Double value : valuesToCheck) {
+			if (Double.isNaN(value) || Double.isInfinite(value)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		System.out
+				.println(Double.NEGATIVE_INFINITY == Double.NEGATIVE_INFINITY);
+	}
+
+}
