0cac0f85ffc1331332552c672dbd7aa81ecff975
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2014-12-12 13:58:40 +0100

    Also support assertion serialization to longtable

--- src/main/java/bpp/util/LatexSerializer.java
+++ src/main/java/bpp/util/LatexSerializer.java
@@ -2,13 +2,14 @@ package bpp.util;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import bpp.domain.assertions.TestAssertion;
 import bpp.domain.assertions.TestAssertions;
 
 public class LatexSerializer {
 
-	private final String FILE_NAME = "assertions.tex";
+	private final String fileName;
 
 	private final String TABLE_NEWLINE = "\\\\";
 
@@ -21,79 +22,139 @@ public class LatexSerializer {
 	private int floatsProcessed;
 
 	public static void main(String[] args) throws FileNotFoundException {
-		new LatexSerializer(true).serializeAssertions();
+		new LatexSerializer(true, args[0]).serializeAssertions(true);
 	}
 
-	LatexSerializer(boolean useColor) {
+	LatexSerializer(boolean useColor, String fileName) {
 		this.useRowColor = useColor;
+		this.fileName = fileName;
 	}
 
-	private void serializeAssertions() throws FileNotFoundException {
+	public void serializeAssertions(boolean useLongTable)
+			throws FileNotFoundException {
 		try {
-			writer = new PrintWriter(FILE_NAME);
+			writer = new PrintWriter(fileName);
 
-			floatsProcessed = 0;
-
-			for (TestAssertion assertion : new TestAssertions().createAll()) {
-
-				serializeAssertion(assertion);
-				checkForTooManyFloats();
+			if (useLongTable) {
+				serializeAssertionsToLongTable();
+			} else {
+				serializeAssertionsToMultipleTables();
 			}
+
 		} finally {
 			if (writer != null) {
 				writer.close();
 			}
 		}
-
 	}
 
-	private void serializeAssertion(TestAssertion assertion) {
-		writeHeader(assertion);
-		writeBody(assertion);
-		writeFooter(assertion);
+	private void serializeAssertionsToMultipleTables()
+			throws FileNotFoundException {
+		
+			floatsProcessed = 0;
+
+			for (TestAssertion assertion : new TestAssertions().createAll()) {
+				writeMultiTableAssertion(assertion);
+				checkForTooManyFloats();
+			}
 	}
 
-	private void writeHeader(TestAssertion assertion) {
-		writer.println("\\begin{table}[htbp]");
-		writer.println("\\center");
-		writer.println("\\scriptsize");
-		writer.println("\\caption{Test Assertion " + assertion.getId() + "}");
-		writer.println("\\begin{tabular}{p{0.1\\textwidth}|p{0.84\\textwidth}}");
-		writer.println("\\toprule");
+	private void serializeAssertionsToLongTable() throws FileNotFoundException {
+
+			writeLongTableHeader();
+
+			writeLongTableBody();
+
+			writeLongTableFooter();
 	}
 
-	private void writeBody(TestAssertion assertion) {
-		writer.println("ID & " + assertion.getId() + TABLE_NEWLINE);
+	private void writeLongTableHeader() {
+		println("\\begin{center}");
+		println("\\begin{scriptsize}");
+		println("\\begin{longtable}{p{0.1\\textwidth}|p{0.84\\textwidth}}");
+		println("\\caption{Portability Test Assertions}\\\\");
+		println("\\textbf{Property} & \\textbf{Test Assertion}");
+		println("\\vspace{2pt}\\\\");
+		println("\\toprule");
+		println("\\endfirsthead");
+		println("\\multicolumn{2}{c}");
+		println("{\\tablename\\ \\thetable\\ -- \\emph{Continued from previous page}}\\\\");
+		println("\\endhead");
+		println("\\multicolumn{2}{r}{\\emph{Continued on next page}}");
+		println("\\endfoot");
+		println("\\bottomrule");
+		println("\\endlastfoot");
+	}
+
+	private void writeLongTableBody() {
+		List<TestAssertion> assertions = new TestAssertions().createAll();
+		for (int i = 0; i < assertions.size(); i++) {
+			// body
+			writeMultiTableBody(assertions.get(i));
+
+			// print a midrule expect for last assertion
+			if (i != assertions.size() - 1) {
+				println("\\midrule");
+			}
+		}
+	}
+
+	private void writeLongTableFooter() {
+		println("\\end{longtable}");
+		println("\\end{scriptsize}");
+		println("\\end{center}");
+	}
+
+	private void writeMultiTableAssertion(TestAssertion assertion) {
+		writeMultiTableHeader(assertion);
+		writeMultiTableBody(assertion);
+		writeMultitableFooter(assertion);
+	}
+
+	private void writeMultiTableHeader(TestAssertion assertion) {
+		println("\\begin{table}[htbp]");
+		println("\\center");
+		println("\\scriptsize");
+		println("\\caption{Test Assertion " + assertion.getId() + "}");
+		println("\\begin{tabular}{p{0.1\\textwidth}|p{0.84\\textwidth}}");
+		println("\\toprule");
+	}
+
+	private void writeMultiTableBody(TestAssertion assertion) {
+		println("ID & " + assertion.getId() + TABLE_NEWLINE);
 		printColor();
-		writer.println("Description &" + assertion.getDescription()
-				+ TABLE_NEWLINE);
-		writer.println("Target & " + assertion.getTarget() + TABLE_NEWLINE);
+		println("Description &" + assertion.getDescription() + TABLE_NEWLINE);
+		println("Target & " + assertion.getTarget() + TABLE_NEWLINE);
 		printColor();
-		writer.println("Degree & " + assertion.getDegree() + TABLE_NEWLINE);
-		writer.println("Level & "
+		println("Degree & " + assertion.getDegree() + TABLE_NEWLINE);
+		println("Level & "
 				+ assertion.getLevel().toString().replace("_", "\\_")
 				+ TABLE_NEWLINE);
 	}
 
-	private void writeFooter(TestAssertion assertion) {
-		writer.println("\\bottomrule");
-		writer.println("\\end{tabular}");
-		writer.println("\\label{tab:" + assertion.getId() + "}");
-		writer.println("\\end{table}");
-		writer.println();
+	private void writeMultitableFooter(TestAssertion assertion) {
+		println("\\bottomrule");
+		println("\\end{tabular}");
+		println("\\label{tab:" + assertion.getId() + "}");
+		println("\\end{table}");
+		println("");
 	}
 
 	private void checkForTooManyFloats() {
 		floatsProcessed++;
 		if (floatsProcessed % 15 == 0) {
-			writer.println("\\newpage");
-			writer.println();
+			println("\\newpage");
+			println("");
 		}
 	}
-	
-	private void printColor(){
-		if(useRowColor){
-			writer.println(ROW_COLOR);
+
+	private void printColor() {
+		if (useRowColor) {
+			println(ROW_COLOR);
 		}
+	}
+
+	private void println(String line) {
+		writer.println(line);
 	}
 }
