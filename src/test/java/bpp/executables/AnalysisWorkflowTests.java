2f455686c10e181b035c0b67a1e7147a964e24e9
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2015-06-22 15:37:52 +0200

    Do a more fine-grained test to make sense of travis run

--- src/test/java/bpp/executables/AnalysisWorkflowTests.java
+++ src/test/java/bpp/executables/AnalysisWorkflowTests.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,29 +18,44 @@ public class AnalysisWorkflowTests {
 		AnalysisWorkflow workflow = new AnalysisWorkflow(
 				Paths.get("src/main/resources/testfiles"), true);
 		workflow.start();
-		
-		//first result
-		List<String> expected = purgeTimeStamps(Files.readAllLines(Paths.get("src/test/java/bpp/executables/report1.xml")));
-		List<String> actual = purgeTimeStamps(Files.readAllLines(Paths.get("src/main/resources/testfiles/Test1-report.xml")));
-		
-		assertEquals(expected, actual);
-		
-		//second result
-		expected = purgeTimeStamps(Files.readAllLines(Paths.get("src/test/java/bpp/executables/report2.xml")));
-		actual = purgeTimeStamps(Files.readAllLines(Paths.get("src/main/resources/testfiles/Test2-report.xml")));
-		
-		assertEquals(expected, actual);
+
+		// first result
+		assertFileContentEquality(
+				Paths.get("src/test/java/bpp/executables/report1.xml"),
+				Paths.get("src/main/resources/testfiles/Test1-report.xml"));
+
+		// second result
+		assertFileContentEquality(
+				Paths.get("src/test/java/bpp/executables/report2.xml"),
+				Paths.get("src/main/resources/testfiles/Test2-report.xml"));
 	}
-	
-	private List<String> purgeTimeStamps(List<String> toPurge){
+
+	private void assertFileContentEquality(Path expected, Path actual)
+			throws IOException {
+		List<String> expectedLines = purgeTimeStamps(Files
+				.readAllLines(expected));
+		List<String> actualLines = purgeTimeStamps(Files.readAllLines(actual));
+
+		assertEquals("Expected lines: " + expectedLines.size()
+				+ "; Actual lines: " + actualLines.size(),
+				expectedLines.size(), actualLines.size());
+		for (int i = 0; i < expectedLines.size(); i++) {
+			String expectedLine = expectedLines.get(i);
+			String actualLine = actualLines.get(i);
+			assertEquals("Expected: " + expectedLine + "; Actual: "
+					+ actualLine, expectedLine, actualLine);
+		}
+	}
+
+	private List<String> purgeTimeStamps(List<String> toPurge) {
 		List<String> result = new ArrayList<>(toPurge.size());
-		
-		for(String line: toPurge){
-			if(!line.toLowerCase().contains("timestamp")){
+
+		for (String line : toPurge) {
+			if (!line.toLowerCase().contains("timestamp")) {
 				result.add(line);
 			}
 		}
-		
+
 		return result;
 	}
 
